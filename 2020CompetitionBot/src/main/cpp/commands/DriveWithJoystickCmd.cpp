f0bed2780a2b670a85ab6dc4ed6e11e429a36dbd
4917 Ultron <software@4917.ca>
2020-02-15 11:39:30 -0500

    Added code for acceleration and deceleration

--- 2020CompetitionBot/src/main/cpp/commands/DriveWithJoystickCmd.cpp
+++ 2020CompetitionBot/src/main/cpp/commands/DriveWithJoystickCmd.cpp
@@ -9,34 +9,57 @@
 #include <frc/Joystick.h>
 #include "commands/DriveWithJoystickCmd.h"
 #include "subsystems/DrivetrainSub.h"
-constexpr int kSensativityPower=2;
 
+constexpr int kSensativityPower=2;
+constexpr double kMaxForwardAccel = 0.02;
+constexpr double kMaxTurnAccel = 0.1;
 DriveWithJoystickCmd::DriveWithJoystickCmd(DrivetrainSub* drivetrainSub, frc::Joystick* joystick) : m_drivetrainSub(drivetrainSub), m_joystick(joystick){
   // Use addRequirements() here to declare subsystem dependencies.
   AddRequirements({drivetrainSub});
 }
 
 // Called when the command is initially scheduled.
-void DriveWithJoystickCmd::Initialize() {}
+void DriveWithJoystickCmd::Initialize() {
+  m_forwardPowerPrevious = 0.0;
+  m_turnPowerPrevious = 0.0;
+}
 
+double adjustSensitivity (double power) {
+  double sign=-1.0;
+  if (power >= 0) {
+   sign=1.0;
+  }
+  power=pow(power,kSensativityPower);
+  power=fabs(power)*sign;
+
+  return power;
+}
+
+double capAcceleration (double power, double powerPrevious, double maxAccel){
+  if (powerPrevious - power > maxAccel){
+    power = powerPrevious - maxAccel;
+  }
+  else if (power - powerPrevious > maxAccel){
+    power = powerPrevious + maxAccel;
+  }
+  return power;
+}
 // Called repeatedly when this Command is scheduled to run
 void DriveWithJoystickCmd::Execute() {
-  double y=m_joystick->GetY();
-  double SignY=-1.0;
-  if (y >= 0) {
-   SignY=1.0;
-  }
-  y=pow(y,kSensativityPower);
-  y=fabs(y)*SignY;
-  double z=-m_joystick->GetZ();
-  double SignZ=-1.0;
-  if (z >= 0) {
-   SignZ=1.0;
-  }
-  z=pow(z,kSensativityPower);
-  z=fabs(z)*SignZ;
-  m_drivetrainSub->arcadeDrive(y,z);
+  double forwardPower=m_joystick->GetY();
+  double turnPower=-m_joystick->GetZ();
+
+  forwardPower =  adjustSensitivity (forwardPower);
+  turnPower = adjustSensitivity (turnPower);
+
+  forwardPower = capAcceleration(forwardPower, m_forwardPowerPrevious, kMaxForwardAccel);
+  turnPower = capAcceleration(turnPower, m_turnPowerPrevious, kMaxTurnAccel);
+
+  m_drivetrainSub->arcadeDrive(forwardPower,turnPower);
   m_drivetrainSub->autoShift();
+
+  m_forwardPowerPrevious = forwardPower;
+  m_turnPowerPrevious = turnPower;
  }
 
 // Called once the command ends or is interrupted.
