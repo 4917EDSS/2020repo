fadb902fa202a86f026fe21e5a1615a974d21c0a
Eric Gaudet <ericgaudet@4917.ca>
2020-02-26 16:01:25 -0500

    Just cleaned up the RamseteCmd indentation.  Added TODO (possible reason for crash)

--- 2020CompetitionBot/src/main/cpp/commands/RamseteCmd.cpp
+++ 2020CompetitionBot/src/main/cpp/commands/RamseteCmd.cpp
@@ -8,21 +8,23 @@
 #include "commands/RamseteCmd.h"
 #include <frc/controller/SimpleMotorFeedForward.h>
 
-RamseteCmd::RamseteCmd(Trajectory t, DrivetrainSub* drivetrainSub):
-  frc2::RamseteCommand(t, [&drivetrainSub]() {return drivetrainSub->getPose(); },
-  frc::RamseteController(AutoConstants::kRamseteB,
-                        AutoConstants::kRamseteZeta),
-  frc::SimpleMotorFeedforward<units::meters>(
-    DriveConstants::ks, DriveConstants::kv, DriveConstants::ka),
-  DriveConstants::kDriveKinematics,
-  [&drivetrainSub] {return drivetrainSub->getWheelSpeeds(); },
-  frc2::PIDController(DriveConstants::kPDriveVel, 0, 0),
-  frc2::PIDController(DriveConstants::kPDriveVel, 0, 0),
-  [&drivetrainSub] (auto left, auto right) {drivetrainSub->tankDriveVolts(left, right); },
-  {drivetrainSub}),
-  m_drivetrainSub(drivetrainSub)
-  {}
- void RamseteCmd::End(bool interrupted){
+// TODO: Check that '[&drivetrainSub]' is correct.  I might need to be '[drivetrainSub]' since it's alreayd a pointer.
+RamseteCmd::RamseteCmd(Trajectory t, DrivetrainSub* drivetrainSub)
+  : frc2::RamseteCommand(t, 
+      [&drivetrainSub]() {return drivetrainSub->getPose();},
+      frc::RamseteController(AutoConstants::kRamseteB, AutoConstants::kRamseteZeta),
+      frc::SimpleMotorFeedforward<units::meters>(DriveConstants::ks, DriveConstants::kv, DriveConstants::ka),
+      DriveConstants::kDriveKinematics,
+      [&drivetrainSub] {return drivetrainSub->getWheelSpeeds();},
+      frc2::PIDController(DriveConstants::kPDriveVel, 0, 0),
+      frc2::PIDController(DriveConstants::kPDriveVel, 0, 0),
+      [&drivetrainSub](auto left, auto right) {drivetrainSub->tankDriveVolts(left, right);},
+      {drivetrainSub}),
+  m_drivetrainSub(drivetrainSub) {
+
+}
+
+void RamseteCmd::End(bool interrupted) {
   frc2::RamseteCommand::End(interrupted);
   m_drivetrainSub->tankDrive(0, 0);
- }
+}
