8b31667e0f0f1d3dcc3d6aa9ef21c16b542c0aa7
4917 Jarvis <software@4917.ca>
2020-02-29 10:18:26 -0500

    cool proportional stuff

--- 2020CompetitionBot/src/main/include/components/ProportionalController.h
+++ 2020CompetitionBot/src/main/include/components/ProportionalController.h
@@ -3,12 +3,14 @@
 // Once at control-start-delta, output power will be proportional to how close current is to target
 class ProportionalController {
  public:
-  ProportionalController(double targetLocation, double controlStartDelta, double minPower, double maxPower);
+  ProportionalController(double targetLocation, double controlStartDelta, double tolerance, double minPower, double maxPower);
   double getPower(double curLocation);
 
  private:
   double m_targetLocation;      // Where we are trying to get to (any unit)
   double m_controlStartDelta;   // Delta (target - current location) above this value will output max power (same units as current and target)
+  double m_tolerance;           // Point above or below target where the motors will turn off (degrees or encoder ticks)
   double m_minPower;            // Power at which the mechanism moves slightly (0.0 to 1.0)
   double m_maxPower;            // Maximum absolute power that you want the mechanism to use (0.0 to 1.0)  
+  
 };
